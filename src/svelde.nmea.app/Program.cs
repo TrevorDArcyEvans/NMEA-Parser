1094f2ca6bfa9bb586cd0aead09439f88c7cd5ef
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-04-16 13:22:20 +0100

    - remove Azure dependency as this is personal

--- src/svelde.nmea.app/Program.cs
+++ src/svelde.nmea.app/Program.cs
@@ -1,161 +1,145 @@
-﻿using Microsoft.Azure.Devices.Client;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using svelde.nmea.parser;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace svelde.nmea.app
 {
-    public class Program : IDisposable
+  public class Program : IDisposable
+  {
+    private static DateTime _lastSent;
+
+    private static NmeaParser _parser;
+
+    private static StreamWriter _streamWriter;
+
+    private static SerialReader _serialReader;
+
+    public static void Main(string[] args)
     {
-        private static DateTime _lastSent;
+      _lastSent = DateTime.Now;
 
-        private static NmeaParser _parser;
+      var utc = DateTime.UtcNow;
+      var fileName = $"{utc.Year}-{utc.Month}-{utc.Day}={utc.Hour}-{utc.Minute}-{utc.Second}.log";
+      _streamWriter = File.AppendText(fileName);
 
-        private static StreamWriter _streamWriter;
+      Console.WriteLine("Read serial port");
 
-        private static DeviceClient _deviceClient;
+      _parser = new NmeaParser();
 
-        private static SerialReader _serialReader;
+      _parser.NmeaMessageParsed += NmeaMessageParsed;
 
-        public static void Main(string[] args)
-        {
-            _lastSent = DateTime.Now;
+      // TODO   support passing in port from command line
+      _serialReader = new SerialReader();
 
-            var utc = DateTime.UtcNow;
-            var fileName = $"{utc.Year}-{utc.Month}-{utc.Day}={utc.Hour}-{utc.Minute}-{utc.Second}.log";
-            _streamWriter = File.AppendText(fileName);
+      _serialReader.NmeaSentenceReceived += NmeaSentenceReceived;
 
-            try
-            {
-                var connectionString = "HostName=iotc-6e7ea251-71bd-4024-a0a8-a8895ea79b7f.azure-devices.net;DeviceId=GeoTracker1;SharedAccessKey=a99ZyFyYPR0tQl7vWK75y4C3YveuQmK118LYEgsYC5c=";
+      _serialReader.Open();
 
-                _deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+      Console.WriteLine("Initialized...");
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception during IoT connection {ex}");
-            }
-
-            Console.WriteLine("Read serial port");
-
-            _parser = new NmeaParser();
-
-            _parser.NmeaMessageParsed += NmeaMessageParsed;
-
-            _serialReader = new SerialReader();
-
-            _serialReader.NmeaSentenceReceived += NmeaSentenceReceived;
-
-            _serialReader.Open();
-
-            Console.WriteLine("Initialized...");
-
-            Console.ReadKey();
-        }
-
-        private static void NmeaMessageParsed(object sender, NmeaMessage e)
-        {
-            var @switch = new Dictionary<Type, Action> {
-                { typeof(GnggaMessage), () => { Console.WriteLine($"{e}"); } },
-                { typeof(GpggaMessage), () => { Console.WriteLine($"{e}"); } },
-                { typeof(GngllMessage), () => { Console.WriteLine($"{e}"); } },
-                { typeof(GngsaMessage), () => { Console.WriteLine($"{e}"); } },
-                { typeof(GpgsaMessage), () => { Console.WriteLine($"{e}"); } },
-                { typeof(GnrmcMessage), () => { SendMessage(e); } },
-                { typeof(GprmcMessage), () => { SendMessage(e); } },
-                { typeof(GntxtMessage), () => { Console.WriteLine($"{e}"); } },
-                { typeof(GnvtgMessage), () => { Console.WriteLine($"{e}"); } },
-                { typeof(GpvtgMessage), () => { Console.WriteLine($"{e}"); } },
-                { typeof(GpgsvMessage), () => { Console.WriteLine($"{e}(GPS)"); } },
-                { typeof(GlgsvMessage), () => { Console.WriteLine($"{e}(Glosnass)"); } },
-                { typeof(GbgsvMessage), () => { Console.WriteLine($"{e}(Baidoo)"); } },
-            };
-
-            @switch[e.GetType()]();
-        }
-
-        private static void SendMessage(NmeaMessage e)
-        {
-            Console.WriteLine($"{e}");
-
-            if (_deviceClient == null)
-            {
-                return;
-            }
-
-            if (_lastSent >= DateTime.Now.AddSeconds(-10))
-            {
-                return;
-            }
-
-            try
-            {
-                if (!(e is GnrmcMessage) 
-                       || !(e as GnrmcMessage).ModeIndicator.IsValid())
-                {
-                    Console.WriteLine($"*** Invalid fix '{(e as GngllMessage)?.ModeIndicator}'; no location sent");
-                    return;
-                }
-
-                _lastSent = DateTime.Now;
-
-                var telemetry = new Telemetry
-                {
-                    Location = new TelemetryLocation
-                    {
-                        Latitude = (e as GnrmcMessage).Latitude.ToDecimalDegrees(),
-                        Longitude = (e as GnrmcMessage).Longitude.ToDecimalDegrees(),
-                    },
-                    FixTaken = (e as GnrmcMessage).TimeOfFix,
-                    ModeIndicator = (e as GnrmcMessage).ModeIndicator,
-                };
-
-                var json = JsonConvert.SerializeObject(telemetry);
-
-                var message = new Message(Encoding.ASCII.GetBytes(json));
-
-                _deviceClient.SendEventAsync(message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception during IoT communication {ex}");
-            }
-        }
-
-        private static void NmeaSentenceReceived(object sender, NmeaSentence e)
-        {
-            _streamWriter.WriteLine(e.Sentence);
-
-            _parser.Parse(e.Sentence);
-        }
-
-        public void Dispose()
-        {
-            _serialReader?.Dispose();
-        }
+      Console.ReadKey();
     }
 
-    public class Telemetry
+    private static void NmeaMessageParsed(object sender, NmeaMessage e)
     {
-        [JsonProperty(PropertyName = "location")]
-        public TelemetryLocation Location { get; set; }
-        [JsonProperty(PropertyName = "modeIndicator")]
-        public ModeIndicator ModeIndicator { get; set; }
-        [JsonProperty(PropertyName = "fixTaken")]
-        public string FixTaken { get; set; }
+      var @switch = new Dictionary<Type, Action>
+      {
+        {typeof(GnggaMessage), () => { Console.WriteLine($"{e}"); }},
+        {typeof(GpggaMessage), () => { Console.WriteLine($"{e}"); }},
+        {typeof(GngllMessage), () => { Console.WriteLine($"{e}"); }},
+        {typeof(GngsaMessage), () => { Console.WriteLine($"{e}"); }},
+        {typeof(GpgsaMessage), () => { Console.WriteLine($"{e}"); }},
+        {typeof(GnrmcMessage), () => { SendMessage(e); }},
+        {typeof(GprmcMessage), () => { SendMessage(e); }},
+        {typeof(GntxtMessage), () => { Console.WriteLine($"{e}"); }},
+        {typeof(GnvtgMessage), () => { Console.WriteLine($"{e}"); }},
+        {typeof(GpvtgMessage), () => { Console.WriteLine($"{e}"); }},
+        {typeof(GpgsvMessage), () => { Console.WriteLine($"{e}(GPS)"); }},
+        {typeof(GlgsvMessage), () => { Console.WriteLine($"{e}(Glosnass)"); }},
+        {typeof(GbgsvMessage), () => { Console.WriteLine($"{e}(Baidoo)"); }},
+      };
+
+      @switch[e.GetType()]();
     }
 
-    public class TelemetryLocation
+    private static void SendMessage(NmeaMessage e)
     {
-        [JsonProperty(PropertyName = "lat")]
-        public decimal Latitude { get; set; }
-        [JsonProperty(PropertyName = "lon")]
-        public decimal Longitude { get; set; }
-        [JsonProperty(PropertyName = "alt")]
-        public decimal? Altitude { get; set; }
+      Console.WriteLine($"{e}");
+
+      if (_lastSent >= DateTime.Now.AddSeconds(-10))
+      {
+        return;
+      }
+
+      try
+      {
+        if (!(e is GnrmcMessage)
+            || !(e as GnrmcMessage).ModeIndicator.IsValid())
+        {
+          Console.WriteLine($"*** Invalid fix '{(e as GngllMessage)?.ModeIndicator}'; no location sent");
+          return;
+        }
+
+        _lastSent = DateTime.Now;
+
+        var telemetry = new Telemetry
+        {
+          Location = new TelemetryLocation
+          {
+            Latitude = (e as GnrmcMessage).Latitude.ToDecimalDegrees(),
+            Longitude = (e as GnrmcMessage).Longitude.ToDecimalDegrees(),
+          },
+          FixTaken = (e as GnrmcMessage).TimeOfFix,
+          ModeIndicator = (e as GnrmcMessage).ModeIndicator,
+        };
+
+        var json = JsonConvert.SerializeObject(telemetry);
+
+        // TODO     SendMessage
+        // var message = new Message(Encoding.ASCII.GetBytes(json));
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Exception during IoT communication {ex}");
+      }
     }
+
+    private static void NmeaSentenceReceived(object sender, NmeaSentence e)
+    {
+      _streamWriter.WriteLine(e.Sentence);
+
+      _parser.Parse(e.Sentence);
+    }
+
+    public void Dispose()
+    {
+      _serialReader?.Dispose();
+    }
+  }
+
+  public class Telemetry
+  {
+    [JsonProperty(PropertyName = "location")]
+    public TelemetryLocation Location { get; set; }
+
+    [JsonProperty(PropertyName = "modeIndicator")]
+    public ModeIndicator ModeIndicator { get; set; }
+
+    [JsonProperty(PropertyName = "fixTaken")]
+    public string FixTaken { get; set; }
+  }
+
+  public class TelemetryLocation
+  {
+    [JsonProperty(PropertyName = "lat")]
+    public decimal Latitude { get; set; }
+
+    [JsonProperty(PropertyName = "lon")]
+    public decimal Longitude { get; set; }
+
+    [JsonProperty(PropertyName = "alt")]
+    public decimal? Altitude { get; set; }
+  }
 }
